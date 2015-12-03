e53d778804fa0905c9df50932d4ba25352a6b50d
syachi <1040899365@qq.com>
2015-12-03 19:36:14 +0800

    content of class 2 updated

---
+++ 崔景育/class2/static/js/HINT.js
@@ -0,0 +1,6 @@
+//恭喜你！找到了隐藏的输入框
+/*
+$(function(){
+	$('#HINT').after('<div class="form-group"><label for="comments">对NIMO或NIMO的网站有什么想说的？</label><textarea class="form-control" id="comments" rows="3"></textarea></div>');
+});
+*/
